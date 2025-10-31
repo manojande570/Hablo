b4e1cb28b02108145a050caae4a40ad542c02f43
bhavya570 <b.vaddepally@itribe.in>
2025-10-31 20:00:12 +0530

    firebase deplotment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 // import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
         domains: ['www.habloindia.com'], // ✅ Allow your logo domain
     },
